7e2c4a6751b1d91b4e8fe7c35fcbc745fd8bf347
Maleeki <slyman7311@gmail.com>
2023-06-01 04:30:25 +0200

    yup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import "./App.css"
 function App() {
   return (
-    <div className="App">
-        Subscribe Zainkeeps code
-    </div>
+    <div className="App">Maleek Keeps Code!</div>
   );
 }
 
